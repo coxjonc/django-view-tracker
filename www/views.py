05dbb948afb078eedf691f2d146413aaa93d3e91
geezhawk <jonathan.cox.c@gmail.com>
2016-02-09 00:11:20 +0700

    changes

--- www/views.py
+++ www/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 from .models import Byline
 
 def scores(request):
-    return render(request, 'www/index.html', {'bylines':Byline.objects.all()})
+    return HttpResponse('ok')
+    #return render(request, 'www/index.html', {'bylines':Byline.objects.all()})
