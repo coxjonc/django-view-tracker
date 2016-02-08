99202e95673e4d47579b76b1051b9631e080e797
geezhawk <jonathan.cox.c@gmail.com>
2016-02-08 23:55:47 +0700

    make it better

--- www/management/commands/scraper.py
+++ www/management/commands/scraper.py
@@ -3,8 +3,7 @@ import datetime
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from parsers import kt
-
+from www.parsers import kt
 from www.models import Article, Byline
 
 
