3473cfe6cc8bcb8971415f0bff95a9bba2dcfc5b
geezhawk <jonathan.cox.c@gmail.com>
2016-02-29 09:52:52 +0700

    asdf

--- three_digit/settings.py
+++ three_digit/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+print PROJECT_ROOT
 
 
 # Quick-start development settings - unsuitable for production
